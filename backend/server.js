af80d39dac7ef388fc9665687812cbce67e655cc
milviobello <milviobello@gmail.com>
2023-03-18 13:05:59 -0400

    CORS support

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,19 @@ const app = express()
 // middleware
 app.use(express.json()) // Allows access to req.body within the varias requests.
 
+const allowedDomains = ['https://codebyte-finalpresentation.onrender.com/', 'http://localhost:3000'];
+
 app.use(cors({
-    origin: 'http://localhost:3000'
+  origin: function (origin, callback) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+ 
+    if (allowedDomains.indexOf(origin) === -1) {
+      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 app.use((req, res, next) => {
